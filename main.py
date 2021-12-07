576e34fd857038c125c24fc559b3891a12dda05d
Alex Wardill <alexander@wardill.net>
2021-12-07 19:56:24 +0000

    working doubling function from external module. doesn't reset page on load...

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-from flask import Flask, render_template
-from simple import write_n_lines
+from flask import Flask, render_template, request, session
+# from simple import write_n_lines
+from adder import double
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mvgtwiey4528wivndt6r89wjsnzgsoru56"
 
 @app.route("/", methods=["GET", "POST"])
-def index():
-    return render_template('base.html')
+def home():
+    if "input" not in session:
+        session["input"] = []
 
-@app.route("/split")
-def split():
-    return render_template('split.html')
+    errors = ""
+
+    if request.method == "POST":
+        number = None
+        try:
+            session["input"].append(int(request.form["number"]))
+            session.modified = True
+        except:
+            errors += f"!{number} is not a number."
+        if request.form.getlist("action")[0] == "Double it":
+            result = double(session["input"][0])
+            session["input"].clear()
+            session.modified = True
+            return render_template('split.html', errors=errors, result=result)
+    return render_template('split.html', errors=errors, result=None)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
-@app.route("/write-lines")
-def writeLines():
-    return
