eae615e67fef0fe9c465239ee2aa9e09cfbad190
Alex Wardill <alexander@wardill.net>
2021-12-08 01:29:49 +0000

    tried to tweak pdfSplitter() to return responses

--- Splitter.py
+++ Splitter.py
@@ -1,18 +1,26 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
 from pathlib import Path
+from flask import request
 
-def pdfSplitter(big_pdf_name):
+def pdfSplitter(target_pdf):
     pdf_path = (
             Path.home()
-            / "PycharmProjects"
-            / "PDFSplitter"
-            / f"{big_pdf_name}"
+            / "Downloads"
+            / f"{target_pdf}"
     )
     input_pdf = PdfFileReader(str(pdf_path))
 
+    
     for i in range(input_pdf.getNumPages()):
         pdf_writer = PdfFileWriter()
-        pdf_writer.addPage(input_pdf.getPage(i))
-        with Path(f"output_files/page {i+1}.pdf").open(mode="wb") as output_file:
-            pdf_writer.write(output_file)
-    return 0
+        response = pdf_writer.addPage(input_pdf.getPage(i))
+        response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+        return response
+    # for i in range(input_pdf.getNumPages()):
+    #     pdf_writer = PdfFileWriter()
+    #     pdf_writer.addPage(input_pdf.getPage(i))
+    #     with Path(f"Downloads/{target_pdf} page {i+1}.pdf").open(mode="wb") as output_file:
+    #         pdf_writer.write(output_file)
+    # return 0
+
+    # return render_template('split.html')
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, make_response
+from Splitter import pdfSplitter
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 app.config["SECRET_KEY"] = "mvgtwiey4528wivndt6r89wjsnzgsoru56"
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    
+    if request.method == "POST":
+        the_request = request.files
+        print(f"REQUEST IS {the_request}")
+        input_file = request.files["input_file"]
+        pdfSplitter(input_file)
     return render_template('split.html')
 
 
