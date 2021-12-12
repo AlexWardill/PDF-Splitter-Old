eb0816f6857ecd00a529c67c55994c868f4de3ba
Alex Wardill <alexander@wardill.net>
2021-12-12 00:09:30 +0000

    added blank_file target

--- src/main.py
+++ src/main.py
@@ -37,8 +37,8 @@ def upload_file():
                 pdf_writer.addPage(big_pdf.getPage(i))
                 # filename = secure_filename(f"page {i+1} {file.filename}")
                 # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                
-                with Path(os.path.join(Path.home(), f"Downloads" + f"page {i+1} {file.filename}")).open(mode="wb") as output_file:
+                blank_file = PdfFileWriter()
+                with Path(os.path.join(Path.home(), f"Downloads" + f"{blank_file}")).open(mode="wb") as output_file:
                     pdf_writer.write(output_file)
                     
             return redirect(url_for('upload_file'))
